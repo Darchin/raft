ba3839770369c0df80330d464375ac89d9e48bfe
Cinnamon <mix3d.r@gmail.com>
2024-05-13 22:43:18 +0330

    minor bug fix

--- messages/message.py
+++ messages/message.py
@@ -1,39 +1,42 @@
 class Message:
-    Generic = 0
+    # Usage of 'Message' parent class allows addition of certain global features like timestamps.
+    Broadcast = 0
     LogRequest = 1
     LogResponse = 2
     VoteRequest = 3
     VoteResponse = 4
 
-    def __init__(self, type, **kwargs):
-        self._type = type
+class Broadcast(Message):
+    def __init__(self, record):
+        self._record = record
 
-        match type:
-            
-            case Message.Generic:
-                self.__dict__.update(kwargs)
+class LogRequest(Message):
+    def __init__(self, leader_id, term, prefix_len, prefix_term, leader_commit, suffix):
+        self._leader_id = leader_id
+        self._term = term
+        self._prefix_len = prefix_len
+        self._prefix_term = prefix_term
+        self._leader_commit = leader_commit
+        self._suffix = suffix
+        
+class LogResponse(Message):
+    def __init__(self, follower_id, term, ack, success):
+        self._type = Message.LogResponse
+        self._follower_id = follower_id
+        self._term = term
+        self._ack = ack
+        self._success = success
 
-            case Message.LogRequest:
-                self.leader_id = kwargs['leader_id']
-                self.term = kwargs['term']
-                self.prefix_len = kwargs['prefix_len']
-                self.prefix_term = kwargs['prefix_term']
-                self.leader_commit = kwargs['leader_commit']
-                self.suffix = kwargs['suffix']
-            
-            case Message.LogResponse:
-                self.follower = kwargs['follower']
-                self.term = kwargs['term']
-                self.ack = kwargs['ack']
-                self.success = kwargs['success']
 
-            case Message.VoteRequest:
-                self.candidate_id = kwargs['candidate_id']
-                self.candidate_term = kwargs['candidate_term']
-                self.candidate_log_length = kwargs['candidate_log_length']
-                self.candidate_log_term = kwargs['candidate_log_term']
-            
-            case Message.VoteResponse:
-                self.voter_id = kwargs['voter_id']
-                self.term = kwargs['term']
-                self.granted = kwargs['granted']
+class VoteRequest(Message):
+    def __init__(self, candidate_id, candidate_term, candidate_log_length, candidate_log_term):
+        self._candidate_id = candidate_id
+        self._candidate_term = candidate_term
+        self._candidate_log_length = candidate_log_length
+        self._candidate_log_term = candidate_log_term
+
+class VoteResponse(Message):
+    def __init__(self, voter_id, term, granted):
+        self._voter_id = voter_id
+        self._term = term
+        self._granted = granted
