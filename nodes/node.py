1175e8de0d6372cbe03743bb46c77027fabc5be3
Cinnamon <mix3d.r@gmail.com>
2024-05-13 22:43:51 +0330

    change of structure, moved heartbeat to state

--- nodes/node.py
+++ nodes/node.py
@@ -1,15 +1,11 @@
-import threading
-import time
+from ..states.state import State
+from ..messages.message import *
 
 class Node():
     # Time between every heartbeat in seconds
     Heartbeat_period = 0.1
-    def __init__(self):
-
-        threading.Thread(target=self.periodic_heartbeat, args=self)
-        pass
-
-    def periodic_heartbeat(self):
-        while self._role == 'leader':
-            # send empty log to all other nodes
-            time.sleep(0.1)
+    def __init__(self, id, node_ids: set):
+        self._id = id
+        self._node_ids = node_ids
+        self._state = State(server=self)
+        # self._state.assign_to_server(self)
