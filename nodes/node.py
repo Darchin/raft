387dc8bc897e534c03804d72cdbf052894423ec5
Cinnamon <mix3d.r@gmail.com>
2024-05-14 00:07:25 +0330

    added support for instruction delivery from states

--- nodes/node.py
+++ nodes/node.py
@@ -4,8 +4,9 @@ from ..messages.message import *
 class Node():
     # Time between every heartbeat in seconds
     
-    def __init__(self, id, node_ids: set):
+    def __init__(self, id, node_ids: set, instructions):
         self._id = id
         self._node_ids = node_ids
         self._state = State(server=self)
+        self._instructions = instructions
         # self._state.assign_to_server(self)
